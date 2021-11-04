53e1eba963606ba6992b1647a7eb12c801c20449
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2021-11-04 13:30:11 -0400

    More documentation

--- config.py
+++ config.py
@@ -79,7 +79,10 @@ class DiffGeneExpDialog(QtWidgets.QDialog):
 
     def _apply(self):
         """
-        Actually make the subset from the differential gene expression calculation
+        Calculate differential gene expression between the two selected
+        subsets and create a new subset_group with the genes that are
+        differentially expressed. Currently this calculation is just
+        genes that show 2x expression in one or other subset.
         """
         for subset1 in self.state.subset1.subsets:
             if subset1.data == self.state.data:
@@ -103,7 +106,7 @@ class DiffGeneExpDialog(QtWidgets.QDialog):
         state_list = []
         for gene in gene_list:
             # Sum over gene expression values, divide by length of subset to get
-            # the average value for this SPARSE array
+            # the average value for this SPARSE array (missing = 0)
             g1 = np.sum(gene_expression_set1[geneset1 == gene])/len1
             g2 = np.sum(gene_expression_set2[geneset2 == gene])/len2
             
@@ -112,7 +115,7 @@ class DiffGeneExpDialog(QtWidgets.QDialog):
                 state_list.append(self.state.data.id[self.state.gene_att] == gene)
         print(differential_genes)
         final_state = MultiOrState(state_list)
-        self.state.data_collection.new_subset_group('Diff Gene', final_state)
+        self.state.data_collection.new_subset_group('Differentially expressed genes', final_state)
 
     @classmethod
     def create_subset(cls, collect, default=None, parent=None):
